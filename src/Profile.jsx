09c4fd4ef32403ac528abe72436d228d4a0cfe5b
Jesse2308 <jrnungaray@hotmail.com>
2024-01-22 17:22:52 -0700

    Finished

---
+++ src/Profile.jsx
@@ -0,0 +1,13 @@
+const Profile = () => {
+  return (
+    <section>
+      <h2>Jesse Nungaray Jr</h2>
+      <img src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:" />
+      <address>
+        <a href="mailto:jrnungaray@hotmail.com">jrnungaray@hotmail.com</a>
+        <br /> <a href="tel:+9516660924">(951)666-0924</a>
+      </address>
+    </section>
+  );
+};
+export default Profile;
